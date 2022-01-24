947b62a4a2adc5c882dab2b2b64c8a55b3d4740f
alfiansyahichsan <alfiansyah.ichsan@gmail.com>
2022-01-24 18:23:36 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>ajojing</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>
+
+<style lang="postcss" scoped>
+div {
+  @apply bg-red-200;
+  h1 {
+    @apply text-xl font-bold;
+  }
+}
+</style>
